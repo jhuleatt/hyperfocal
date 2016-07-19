15c316e37539122fe09394f3288b9216259558da
Jeff Huleatt <jhuleatt@advent.com>
2016-07-18 18:37:57 -0700

    ignore deleted files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -48,6 +48,7 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 # The delta will show diffs between new and old versions.
 git add --ignore-removal .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
+git checkout -- .
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
