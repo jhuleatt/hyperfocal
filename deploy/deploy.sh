eca295b62d7219a09dc0e2ca4dc7ffc6403dc83d
Jeff Huleatt <jhuleatt@advent.com>
2016-07-18 17:14:13 -0700

    trying different path for compile script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,8 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  pwd
-  deploy/compile.sh
+  ./deploy/compile.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
