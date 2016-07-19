b411a7b9899b14e80ab9c3968112aace5ec77ace
jhuleatt <jhuleatt@gmail.com>
2016-07-18 20:43:39 -0700

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,26 +20,13 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-pwd
-ls
-echo "SUP"
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
 # Run our compile script
 doCompile
 
-# Now let's go have some fun with the cloned repo
-cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-git status
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
