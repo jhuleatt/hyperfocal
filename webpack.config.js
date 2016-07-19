b08be5510e7e9582196624e8c597776b4ced4be0
Jeff Huleatt <jhuleatt@advent.com>
2016-07-19 15:39:30 -0700

    lint tsx too!

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 loader: "source-map-loader"
             },
             {
-                test: /\.ts$/,
+                test: /\.tsx?$/,
                 loader: "tslint"
             }
         ]
