4444a80d3e163b13396882f121d938acc970db29
jhuleatt <jhuleatt@gmail.com>
2016-07-18 20:40:49 -0700

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,20 +20,17 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+pwd
+ls
+echo "SUP"
+
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
-echo "YO"
-pwd
-ls
-echo "WASSUP"
-cd out
-pwd
-ls
-cd ..
+
 # Run our compile script
 doCompile
 
