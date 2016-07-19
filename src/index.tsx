51bd232738812262883a476b6d0ee28aee84fb22
jhuleatt <jhuleatt@gmail.com>
2016-07-18 20:09:39 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+console.log('WASSUP DAWG');
+
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("example")
