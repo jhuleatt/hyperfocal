de6bd7a4652a3964981638a2a53ada2ca81b9449
jhuleatt <jhuleatt@gmail.com>
2016-07-18 20:24:32 -0700

    Update Hello.tsx

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,6 +4,6 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}! Jeff Test! 123</h1>;
+        return <h1>Hello from {this.props.compiler} and AKJHSKAJSHDKLJAHSD {this.props.framework}! Jeff Test! 123</h1>;
     }
 }
