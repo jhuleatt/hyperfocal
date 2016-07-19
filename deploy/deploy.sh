21c1cfeb67a4226e8b26523355ca3cabe2a449c3
Jeff Huleatt <jhuleatt@advent.com>
2016-07-18 18:55:21 -0700

    fix order in deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -47,8 +47,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add --ignore-removal .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
 git checkout -- .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
