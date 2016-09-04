0ead4d885f7243a2b1221d3a5a287ebd64e54ed7
Jeffrey Huleatt <jhuleatt@scarletmail.rutgers.edu>
2016-09-04 16:07:23 -0700

    fix calculations

--- src/util/calculations.ts
+++ src/util/calculations.ts
@@ -2,6 +2,10 @@
  * Big thank you to http://www.dofmaster.com/equations.html for the formulas
  */
 
+function square(value: number): number {
+  return value * value;
+}
+
 export function calculateHyperfocal(focalLengthMM: number, fNumber: number, confusionMM: number): number {
-  return ( ( focalLengthMM ^ 2 ) / ( fNumber * confusionMM ) ) + focalLengthMM;
+  return ( square(focalLengthMM) / ( fNumber * confusionMM ) ) + focalLengthMM;
 };
